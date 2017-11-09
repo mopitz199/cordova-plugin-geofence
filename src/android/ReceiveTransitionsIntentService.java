7f543d40d6fe601252f8baced759f6a9071aad61
Maximiliano <mopitz@linkq.cl>
2017-11-09 17:18:50 -0300

    mejora para poder manipular las notifiaciones del geofence

--- src/android/ReceiveTransitionsIntentService.java
+++ src/android/ReceiveTransitionsIntentService.java
@@ -2,15 +2,53 @@ package com.cowbell.cordova.geofence;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+
+import java.util.Random;
+
+class TalanaOpenHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "talana";
+    private static final String DICTIONARY_TABLE_NAME = "dictionary";
+    private static final String KEY_WORD = "key";
+    private static final String KEY_DEFINITION = "value";
+    private static final String DICTIONARY_TABLE_CREATE =
+            "CREATE TABLE " + DICTIONARY_TABLE_NAME + " (" +
+                    KEY_WORD + " TEXT, " +
+                    KEY_DEFINITION + " TEXT);";
+
+
+    TalanaOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DICTIONARY_TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // No hay operaciones
+    }
+}
 
 public class ReceiveTransitionsIntentService extends IntentService {
     protected static final String GeofenceTransitionIntent = "com.cowbell.cordova.geofence.TRANSITION";
@@ -42,8 +80,8 @@ public class ReceiveTransitionsIntentService extends IntentService {
         logger.log(Log.DEBUG, "ReceiveTransitionsIntentService - onHandleIntent");
         Intent broadcastIntent = new Intent(GeofenceTransitionIntent);
         notifier = new GeoNotificationNotifier(
-            (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE),
-            this
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE),
+                this
         );
 
         // TODO: refactor this, too long
@@ -71,7 +109,56 @@ public class ReceiveTransitionsIntentService extends IntentService {
 
                     if (geoNotification != null) {
                         if (geoNotification.notification != null) {
-                            notifier.notify(geoNotification.notification);
+                            Calendar cal = Calendar.getInstance();
+                            SimpleDateFormat sdf = new SimpleDateFormat("d");
+
+
+                            TalanaOpenHelper helper = new TalanaOpenHelper(this);
+                            //SQLiteDatabase db = helper.getWritableDatabase();
+
+
+                            /*ContentValues values = new ContentValues();
+                            values.put("key", "nombre");
+                            values.put("value", "maximiliano");
+
+                            long newRowId = db.insert("dictionary", null, values);*/
+
+                            SQLiteDatabase db = helper.getReadableDatabase();
+                            String[] projection = {
+                                    "key",
+                                    "value"
+                            };
+
+                            // Filter results WHERE "title" = 'My Title'
+                            String selection = "key" + " = ?";
+                            String[] selectionArgs = { "notificationDay" };
+
+                            String sortOrder =
+                                    "key" + " DESC";
+
+                            Cursor c = db.query(
+                                    "dictionary",                     // The table to query
+                                    projection,                               // The columns to return
+                                    selection,                                // The columns for the WHERE clause
+                                    selectionArgs,                            // The values for the WHERE clause
+                                    null,                                     // don't group the rows
+                                    null,                                     // don't filter by row groups
+                                    sortOrder                                 // The sort order
+                            );
+
+                            if(c.moveToFirst()){
+                                String key = c.getString(0);
+                                String value = c.getString(1);
+
+                                int notificatedDay = Integer.parseInt(value);
+                                int currentDay = Integer.parseInt(sdf.format(cal.getTime()));
+
+                                if(notificatedDay==0 || notificatedDay!=currentDay){
+                                    notifier.notify(geoNotification.notification);
+                                }
+                            }else{
+                                notifier.notify(geoNotification.notification);
+                            }
                         }
                         geoNotification.transitionType = transitionType;
                         geoNotifications.add(geoNotification);
